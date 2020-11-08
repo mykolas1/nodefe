93e54597f5858da8bf6d3b433cb6571cdeeb6460
mykolas1 <mykolasba9@gmail.com>
2020-11-08 17:44:58 +0200

    1

--- src/main.js
+++ src/main.js
@@ -4,35 +4,34 @@ Vue.config.productionTip = false;
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import routes from "./routes"
-import Router from 'vue-router'
-import Form from "./components/Form";
-import App from "./App";
-import Table from "./components/Table";
+// import Form from "./components/Form";
+// import App from "./App";
+// import Table from "./components/Table";
 Vue.use(Buefy);
-Vue.use(Router);
-
-export default new Router({
-    router: [
-        {
-            path: '/form',
-            name: 'Form',
-            component: Form,
-            props: true
-        },
-        {
-            path: '/home',
-            name: 'Table',
-            component: Table,
-            props: true
-        },
-        {
-            path: '/',
-            name: 'App',
-            component: App,
-            props: true
-        }
-    ]
-});
+// Vue.use(Router);
+//
+// export default new Router({
+//     router: [
+//         {
+//             path: '/form',
+//             name: 'Form',
+//             component: Form,
+//             props: true
+//         },
+//         {
+//             path: '/home',
+//             name: 'Table',
+//             component: Table,
+//             props: true
+//         },
+//         {
+//             path: '/',
+//             name: 'App',
+//             component: App,
+//             props: true
+//         }
+//     ]
+// });
 
 new Vue({
     el: '#app',
